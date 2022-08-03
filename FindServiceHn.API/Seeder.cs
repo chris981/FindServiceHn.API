3b119241e58af0fc7e06d7a912294f5eba7b5b4a
Jorge Alvarez <jorge.edu17@hotmail.com>
2022-08-02 22:45:31 -0600

    update seeder and gitignore

--- FindServiceHn.API/Seeder.cs
+++ FindServiceHn.API/Seeder.cs
@@ -8,16 +8,17 @@ namespace FindServiceHn.API
     {
         public static void Seed(WebApplication app)
         {
-            var testUsers = new List<User>
-            {
-                new User { Id = 1, Username = "admin", PasswordHash = BCryptNet.HashPassword("admin"), Role = Role.Admin },
-                new User { Id = 2, Username = "user", PasswordHash = BCryptNet.HashPassword("user"), Role = Role.User }
-            };
-
             using var scope = app.Services.CreateScope();
             var dataContext = scope.ServiceProvider.GetRequiredService<FindServiceHnContext>();
+
             if (!dataContext.Users.Any())
             {
+                var testUsers = new List<User>
+                {
+                    new User { Id = 1, Username = "admin", PasswordHash = BCryptNet.HashPassword("admin"), Role = Role.Admin },
+                    new User { Id = 2, Username = "user", PasswordHash = BCryptNet.HashPassword("user"), Role = Role.User }
+                };
+
                 dataContext.Users.AddRange(testUsers);
                 dataContext.SaveChanges();
             }
